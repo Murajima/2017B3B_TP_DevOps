8598d17ec2ecb12295750a2cd067ae0dd362a5c4
Antoine <apoussard@pc6.home>
2017-12-25 20:29:36 +0100

    add docker stacks

--- A/docker-entrypoint.sh
+++ A/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 cd /usr/src/app/
 echo "---> Starting in DEV mode"
-NODE_ENV=development MONGO_HOST=localhost MONGO_USER=root MONGO_DATABASE=test MONGO_PASSWORD=123456 node app.js
+NODE_ENV=development MONGO_HOST=52.11.192.36 MONGO_USER=root MONGO_DATABASE=test MONGO_PASSWORD=123456 node app.js
 
 exit 0;
